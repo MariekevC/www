7d12a190dc6b0b15558f685920a7c20223da0535
Tom Needham <needham.thomas@gmail.com>
2012-01-14 14:55:25 +0000

    Inital install, basic theme

---
+++ wp-content/themes/owncloudorg/footer.php
@@ -0,0 +1,10 @@
+			<hr>
+			<footer>
+				<p>&copy; ownCloud <?php echo date('Y'); ?></p>
+			</footer>
+		</div>
+	</div> <!-- /container -->
+	<script src="http://code.jquery.com/jquery-1.5.2.min.js"></script>
+	<?php wp_footer(); ?>
+</body>
+</html>
