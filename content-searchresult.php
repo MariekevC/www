7197002acea150fba787eaf3d757a2292a5e74ac
Tom Needham <needham.thomas@gmail.com>
2012-03-08 22:36:19 +0000

    Update search result styling

--- content-searchresult.php
+++ content-searchresult.php
@@ -1,22 +1,20 @@
 <?php 
-// TODO fix this as has_ancestor() function does not work as intended
-// TODO stop last result from echoing <hr>
-if(has_ancestor($post->ID,'Dev'))
-{
-	$title = '<a href="'.get_permalink($post->ID).'"><h2>Dev Center > '.get_the_title().'</h2></a>';
-}
-elseif(has_ancestor($post->ID,'Support'))
-{
-	$title = '<a href="'.get_permalink($post->ID).'"><h2>Support Center > '.get_the_title().'</h2></a>';
-}
-else
-{
-	$title = '<a href="'.get_permalink($post->ID).'"><h2>'.get_the_title().'</h2></a>';
-}
-echo $title; ?>
-		<?php echo neat_trim(strip_shortcodes(get_the_content()),400); ?>
-		<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>Pages:'. '</span>', 'after' => '</div>' ) );
-if( ($wp_query->current_post + 1) != $wp_query->post_count ) {
-	echo '<hr>';	
-}
+// fixes a bug with post ancestors, patch still not in core release
+// see: http://core.trac.wordpress.org/ticket/10381
+wp_cache_delete($post->ID, 'posts');
+
+// Format title
+if($post->post_type=="post"){
+	echo '<a href="'.get_permalink($post->ID).'"><h2>Dev Blog > '.get_the_title().'</h2></a>';
+} else if(in_array('15',get_post_ancestors($post->ID))){
+	echo '<a href="'.get_permalink($post->ID).'"><h2>Dev Center > '.get_the_title().'</h2></a>';
+} else if(in_array('14',get_post_ancestors($post->ID))){
+	echo '<a href="'.get_permalink($post->ID).'"><h2> Suppport Centre > '.get_the_title().'</h2></a>';
+} else {
+	// Dont show pages that aren't in dev, support, or blog (they must be the core ones at the top)	
+} 
+
+// Content
+echo str_ireplace(get_search_query(),'<strong>'.get_search_query().'</strong>',neat_trim(strip_shortcodes(get_the_content()),400));
+
 ?>
