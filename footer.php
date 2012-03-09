1f6de8c51f23f89b39d7d5b409185d60c45bfbf2
Tom Needham <needham.thomas@gmail.com>
2012-03-09 20:54:12 +0000

    correct footer

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 		<footer>
 			<hr>
-			<small>&#169; ownCloud Inc <?php echo date('Y');?></small>
+			<small>&#169; ownCloud <?php echo date('Y');?></small>
 		</footer>
-		</div>
+	</div>
 <!-- Piwik -->
 <script type="text/javascript">
 var pkBaseURL = (("https:" == document.location.protocol) ? "https://demo.owncloud.org/stats/" : "http://demo.owncloud.org/stats/");
